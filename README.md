50011701fe78e7a8221b8eb40e7dceda643d40b4
Ervin Forth <ervinforthjnr@gmail.com>
2021-01-02 01:47:29 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# todo-app
+#### Todo App
+
+This app is to demonstrate the execution of SQL statements in an application. This is meant for the students of the Intro to Database course.
+
+The app is built using Flask with the Psycopg2 library being used to connect to a Postgrsql database with the following data model.
+
+
+```
+CREATE TABLE todo (
+    id serial not null,
+    name varchar(100) not null,
+    completed boolean not null default false,
+    constraint todo_pkey primary key (id)
+)
+```
+
+#### Environment veriables
+
+Set the DATABASE_URL environment variable on windows by using the following command.
+
+```
+set DATABASE_URL=postgresql://username:password@host:port/database
+```
---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, jsonify, render_template, redirect, request
+import os
+import psycopg2
+from psycopg2 import pool
+
+
+app = Flask(__name__)
+db = psycopg2.pool.SimpleConnectionPool(1, 20, os.environ.get('DATABASE_URL'))
+conn = db.getconn()
+
+@app.route('/')
+def hello():
+    cursor = conn.cursor()
+    cursor.execute("select * from todo order by name asc;")
+    result = cursor.fetchall()
+    cursor.close()
+    return render_template('todo-list.html', todos=result)
+
+@app.route('/todo/create')
+def create():
+    cursor = conn.cursor()
+    if "todo_name" in request.args:
+        todo_name = request.args.get('todo_name')
+        if todo_name:
+            cursor.execute("insert into todo (name,completed) values (%s, %s)", (todo_name, False))
+            conn.commit()
+
+    cursor.close()
+    return redirect('/')
+
+@app.route('/todo/delete/<int:id>')
+def delete(id):
+    cursor = conn.cursor()
+    
+    if id:
+        sql = "delete from todo where id={}".format(id)
+        cursor.execute(sql)
+        conn.commit()
+
+    cursor.close()
+    return redirect('/')
+
+
+@app.route('/todo/complete/<int:id>')
+def complete(id):
+    cursor = conn.cursor()
+
+    if id:
+        sql = "update todo set completed={} where id={}".format(True, id)
+        cursor.execute(sql)
+        conn.commit()
+
+    cursor.close()
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
